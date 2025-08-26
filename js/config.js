4f3fec47ce0525e0800207d126ac5787b0ea621a
Nguyen KY <68001317+dsitweed@users.noreply.github.com>
2025-08-26 12:14:25 +0900

    Update AWS Cognito configuration in config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-northeast-1_Sgym9J9Rn', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '22j0jjis41mpmfsns1lttgfblu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
