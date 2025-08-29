bc0a8994788b595b747c586101df8bb07d28b9ad
Nguyen KY <68001317+dsitweed@users.noreply.github.com>
2025-08-29 15:18:03 +0000

    feate: reset config

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-northeast-1_M2Mp5aeUb', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4tvbhk6uajfb9msmjhvsd5opln', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-northeast-1' // e.g. us-east-2
+        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://anofq96hg0.execute-api.ap-northeast-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
 
